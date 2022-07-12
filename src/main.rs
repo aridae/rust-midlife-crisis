ab3ece0554b0757329b0c4879a10faf102ceac2c
Rusinova Daria Eduardovna <drusinova@ozon.ru>
2022-07-12 23:43:23 +0300

    added simple movements

--- src/main.rs
+++ src/main.rs
@@ -1,163 +1,70 @@
+#![allow(clippy::redundant_field_names)]
+
 //===============================================//
 // U r not special                               //
 // for winning a game                            //
 // with someone who you know was never playing.  //
 //===============================================//
 
-// кажется типа как в шарпах
-use rltk::{GameState, Rltk, RGB, VirtualKeyCode};
-use specs::prelude::*;
-use std::cmp::{max, min};
-use specs_derive::Component;
+use bevy::{prelude::*, render::camera::ScalingMode};
 
-// struct Position {
-//     x: i32,
-//     y: i32,
-// }
-// 
-// impl Component for Position {
-//     type Storage = VecStorage<Self>;
-// }
+mod player;
+use player::PlayerPlugin;
 
+mod debug;
+use debug::DebugPlugin;
 
-// в рамках ECS, указываем что для позишна нужен компонент 
-// то же самое, что и выше, спасибо specs-derive
-// #[derive(x)] is a macro that says "from my basic data, please derive the boilerplate needed for x"
-#[derive(Component)]
-struct Position {
-    x: i32,
-    y: i32,
-}
+pub const CLEAR: Color = Color::rgb(0.1, 0.1, 0.1);
+pub const TILE_SIZE: f32 = 0.1;
 
-#[derive(Component)]
-struct Renderable {
-    glyph: rltk::FontCharType,
-    foreground: RGB,
-    background: RGB,
-}
-
-struct State {
-    ecs: World
-}
-
-#[derive(Component)]
-struct LeftMover {}
-
-struct LeftWalker {}
-
-impl<'a> System<'a> for LeftWalker {
-    type SystemData = (ReadStorage<'a, LeftMover>, 
-                        WriteStorage<'a, Position>);
-
-    fn run(&mut self, (lefty, mut pos) : Self::SystemData) {
-        for (_lefty,pos) in (&lefty, &mut pos).join() {
-            pos.x -= 1;
-            if pos.x < 0 { pos.x = 79; }
-        }
-    }
-}
-
-impl State {
-    fn run_systems(&mut self) {
-        let mut lw = LeftWalker{};
-        lw.run_now(&self.ecs);
-        self.ecs.maintain();
-    }
-}
-
-impl GameState for State {
-    fn tick(&mut self, ctx : &mut Rltk) {
-        ctx.cls();
-
-        self.run_systems();
-        player_input(self, ctx);
-
-        let positions = self.ecs.read_storage::<Position>();
-        let renderables = self.ecs.read_storage::<Renderable>();
-
-        // The join function returns an iterator
-        // like a database join, it only returns entities that have both
-        for (pos, render) in (&positions, &renderables).join() {
-            // ctx is the instance of RLTK passed to us when tick runs
-            ctx.set(pos.x, pos.y, render.foreground, render.background, render.glyph);
-        }
-    }
-}
-
-#[derive(Component, Debug)]
-struct Player {}
-
-
-fn main() -> rltk::BError {
-    use rltk::RltkBuilder;
-
-    let context = RltkBuilder::simple80x50()
-        .with_title("I listen to a lot of True Crime (-.-)")
-        .build()?;
-
-    // mut - mutable
-    let mut game_state = State {
-        ecs: World::new()
-    };
-
-    // Specs requires that you register your components at start-up
-    game_state.ecs.register::<Position>();
-    game_state.ecs.register::<Renderable>();
-    game_state.ecs.register::<LeftMover>();
-    game_state.ecs.register::<Player>();
-
-    // хуяк билдер паттерн
-    game_state.ecs
-        .create_entity()
-        .with(Position { x: 40, y: 25 })
-        .with(Renderable {
-            glyph: rltk::to_cp437('@'),
-            foreground: RGB::named(rltk::YELLOW),
-            background: RGB::named(rltk::BLACK),
+fn main() {
+    App::new()
+        .insert_resource(ClearColor(CLEAR))
+        .insert_resource(WindowDescriptor{
+            width: 1600.0,
+            height: 900.0,
+            title: "I'm really desperate".to_string(), 
+            vsync: true,
+            resizable: false,
+            ..Default::default()
         })
-        .with(Player{})
-        .build();
-
-    // хуяк луп вот так делается 
-    for i in 0..10 {
-        game_state.ecs
-            .create_entity()
-            .with(Position { x: i * 7, y: 20 })
-            .with(Renderable {
-                glyph: rltk::to_cp437('☺'),
-                foreground: RGB::named(rltk::RED),
-                background: RGB::named(rltk::BLACK),
-            })
-            .with(LeftMover{})
-            .build();
-    }
-
-    println!("Hello, world!"); // println! это макро потому что есть !
-
-    rltk::main_loop(context, game_state)
+        .add_startup_system(spawn_camera)
+        .add_startup_system_to_stage(StartupStage::PreStartup, load_ascii)
+        .add_plugins(DefaultPlugins)
+        .add_plugin(PlayerPlugin)
+        .add_plugin(DebugPlugin)
+        .run();
 }
 
+fn spawn_camera(mut commands: Commands) {
+    let mut camera = OrthographicCameraBundle::new_2d();
 
-fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
-    let mut positions = ecs.write_storage::<Position>();
-    let mut players = ecs.write_storage::<Player>();
+    camera.orthographic_projection.top = 1.0;
+    camera.orthographic_projection.bottom = -1.0;
 
-    for (_player, pos) in (&mut players, &mut positions).join() {
-        pos.x = min(79 , max(0, pos.x + delta_x));
-        pos.y = min(49, max(0, pos.y + delta_y));
-    }
+    camera.orthographic_projection.left = 1.0;
+    camera.orthographic_projection.right = -1.0;
+
+    camera.orthographic_projection.scaling_mode = ScalingMode::None;
+
+    commands.spawn_bundle(camera);
 }
 
-fn player_input(gs: &mut State, ctx: &mut Rltk) {
-    // игрока двигаем
-    match ctx.key {
-        None => {} // не двигаем...
-        Some(key) => match key {
-            VirtualKeyCode::Left => try_move_player(-1, 0, &mut gs.ecs),
-            VirtualKeyCode::Right => try_move_player(1, 0, &mut gs.ecs),
-            VirtualKeyCode::Up => try_move_player(0, -1, &mut gs.ecs),
-            VirtualKeyCode::Down => try_move_player(0, 1, &mut gs.ecs),
-            _ => {}
-        },
-    }
+struct AsciiSheet(Handle<TextureAtlas>);
+
+fn load_ascii(
+    mut commands: Commands,
+    assets: Res<AssetServer>,
+    mut texture_atlases: ResMut<Assets<TextureAtlas>>,
+) {
+    let image:Handle<Image> = assets.load("Ascii.png");
+    let atlas = TextureAtlas::from_grid_with_padding(
+        image,
+        Vec2::splat(9.0), 
+        16,
+        16,
+        Vec2::splat(2.0),
+    );
+    let atlas_handle = texture_atlases.add(atlas);
+    commands.insert_resource(AsciiSheet(atlas_handle));
 }
---
+++ src/player.rs
@@ -0,0 +1,54 @@
+use bevy::prelude::*;
+
+use crate::AsciiSheet;
+use crate::TILE_SIZE;
+
+pub struct PlayerPlugin;
+
+#[derive(Component)]
+pub struct Player {
+    speed: f32,
+}
+
+impl Plugin for PlayerPlugin {
+    fn build(&self, app: &mut App) {
+        app
+        .add_startup_system(spawn_player)
+        .add_system(player_movement);
+    }
+}
+
+fn player_movement(
+    mut player_query: Query<(&Player, &mut Transform)>,
+    keyboard: Res<Input<KeyCode>>,
+    time: Res<Time>,
+) {
+    let (player, mut transform) = player_query.single_mut();
+
+    if keyboard.pressed(KeyCode::W) { transform.translation.y += player.speed * TILE_SIZE * time.delta_seconds(); }
+    if keyboard.pressed(KeyCode::S) { transform.translation.y -= player.speed * TILE_SIZE * time.delta_seconds(); }
+    if keyboard.pressed(KeyCode::A) { transform.translation.x -= player.speed * TILE_SIZE * time.delta_seconds(); }
+    if keyboard.pressed(KeyCode::D) { transform.translation.x += player.speed * TILE_SIZE * time.delta_seconds(); }
+}
+
+fn spawn_player(mut commands: Commands, ascii: Res<AsciiSheet>) {
+    let mut sprite = TextureAtlasSprite::new(1);
+    sprite.color = Color::rgb(0.3, 0.3, 0.9);
+    sprite.custom_size = Some(Vec2::splat(TILE_SIZE));
+
+    let _player = commands
+        .spawn_bundle(SpriteSheetBundle{
+            sprite: sprite, 
+            texture_atlas: ascii.0.clone(),
+            transform: Transform {
+                translation: Vec3::new(0.0, 0.0, 900.0),
+                ..Default::default()
+            },
+            ..Default::default()
+        })
+        .insert(Name::new("Player"))
+        .insert(Player {
+            speed: 3.5
+        })
+        .id();
+}
